61143f53ac776a264df6faf7c9be8474afa133cd
evasquez <evasquez@newroads.org>
2017-02-01 12:35:13 -0500

    chicken tenders

--- Board.java
+++ Board.java
@@ -22,5 +22,14 @@ public class Board{
     System.out.println(" " + board[2][0] + " | " +  board[2][1] + " | " + board[2][2] + " ");
   }
   
+  public boolean TTTChecker(){
+    if(board[0][0] == board[0][1] == board[0][2]){
+      return true;
+    }
+    
+    else{
+      return false;
+    }
+  }
   
 }
