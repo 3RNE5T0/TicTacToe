64cfa54864c401d952c4b761e6a8fa5e7bd26b4e
evasquez <evasquez@newroads.org>
2017-02-15 12:31:14 -0500

    yup, we done n done it

--- Board.java
+++ Board.java
@@ -23,10 +23,22 @@ public class Board{
   }
   
   public boolean TTTChecker(){
-    if((board[0][0] == board[0][1]) && (board[0][1] == board[0][2])){
+     if((board[0][0] == board[0][1]) && (board[0][1] == board[0][2]) && board[0][0] == "") 
       return true;
-    }
-    
+else if((board[1][0] == board[1][1]) && (board[1][1] == board[1][2]) && board[1][0] == "")
+return true;
+else if((board[2][0] == board[2][1]) && (board[2][1] == board[2][2]) && board[2][0] == "")
+return true;
+else if((board[0][0] == board[1][0]) && (board[0][0] == board[2][0]) && board[0][0] == "")
+return true; 
+else if((board[0][1] == board[1][1]) && (board[1][1] == board[2][1]) && board[0][1] == "")
+return true; 
+else if((board[0][2] == board[1][2]) && (board[0][2] == board[2][2]) && board[0][0] == "")
+return true; 
+else if((board[0][0] == board[1][1]) && (board[0][0] == board[2][2]) && board[0][0] == "")
+return true; 
+else if((board[0][2] == board[1][1]) && (board[0][2] == board[2][0]) && board[0][2] == "")
+return true;
     else{
       return false;
     }
--- PlayTTT.java
+++ PlayTTT.java
@@ -4,6 +4,17 @@ public class PlayTTT{
   
   public static void main(String[] args){
     
+    
+    
+System.out.println ("                                 ");
+System.out.println ("                _______________ ");
+System.out.println ("                | How to play | ");
+System.out.println ("                | Tic Tac Toe | ");
+System.out.println ("                 ------------- ");
+System.out.println ("                                   ");
+System.out.println ("        1. Give your names. (player 1 is X and 2 is O) ");
+System.out.println ("                        ");
+    
     //scanner for input
     Scanner in = new Scanner(System.in);
     
@@ -21,7 +32,49 @@ public class PlayTTT{
     
     Player player2 = new Player('o', false, nameP2);
     
+    
+    
+    
+    
+    
+    
+System.out.println ( "                                                                                                            ");
+System.out.println ( "    2. player 1 goes first picking a place to put their x ");
+System.out.println ( "                                                                                                            ");
+System.out.println ( "    3. player 2 goes fsecond picking a place to put their o ");
+System.out.println ( "                                                                                                            ");
+System.out.println ( "    4. This process continues until player 1 or 2 gets three of their variebules in a straight line");
+System.out.println ( "                                                                                                            ");
+System.out.println ( "    5. Things to know: ");
+System.out.println ( "        - Do not enter the same #'s as the other player! ");
+System.out.println ( "        - If someone enters something other than a # the game will turn off! ");
+System.out.println ( "        - This is a work in progress game! ");
+System.out.println ( "                                                                                                             ");
+System.out.println ( "                                                                                                             ");
+
+System.out.println ( "                                                     ");
+System.out.println ( "                  0     1     2                      ");
+System.out.println ( "                  |     |     |                      ");
+System.out.println ( "                  v     v     v                      ");
+System.out.println ( "                                                     ");
+System.out.println ( "                     |      |                        ");
+System.out.println ( "                  00 |  01  | 02     <----- 0        ");
+System.out.println ( "               ---------------------                 ");
+System.out.println ( "                     |      |                        ");
+System.out.println ( "                  10 |  11  | 12     <----- 1        ");
+System.out.println ( "               ---------------------                 ");
+System.out.println ( "                     |      |                        ");
+System.out.println ( "                  20 |  21  | 22     <----- 2        ");
+System.out.println ( "                                                     ");
+System.out.println ( "                                                     ");
+
+
+    
+    
+    
     Board board = new Board();
+    
+    System.out.println("Test winner checker: " + board.TTTChecker());
     
     System.out.println("Player " + player1.getName() + ", give row and then column: ");
     int row = in.nextInt();
